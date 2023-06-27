5c92253c3e8ec5bb28f6938fde2d8d621f71a995
Raik Andritschke <58722460+CalliTGS3@users.noreply.github.com>
2023-06-27 15:23:41 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,3 +9,4 @@ let fahrzeug: l298.Vehicle = null
 let motor1 = l298.create_motor(DigitalPin.P0, DigitalPin.P1, AnalogPin.C16)
 let motor2 = l298.create_motor(DigitalPin.P2, DigitalPin.P3, AnalogPin.C17)
 fahrzeug = l298.create_vehicle(motor1, motor2)
+led.enable(false)
